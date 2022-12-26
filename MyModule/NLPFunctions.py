9075e9a7e5d3f464cddebc54e33d16488a24a354
felipeaguirre66 <lipe.aguirre@gmail.com>
2022-12-26 09:49:15 -0300

    added Spacy model

--- MyModule/NLPFunctions.py
+++ MyModule/NLPFunctions.py
@@ -29,7 +29,7 @@ class Preprocess():
     """
     
     def __init__(self, lemma=True, decode=True, alphanumeric=True, stopwords=True, join=True):
-        self.nlp = spacy.load('es_core_news_md')
+        self.nlp = spacy.load('es_core_news_lg')
         
         self.lemma = lemma
         self.decode = decode
@@ -488,8 +488,11 @@ class LSA_Model():
         self.column = data_column
         
         self.training_data = self.df[self.column].tolist()
-    
-        preprocessed_corpus = self.pp_object.preprocess(self.training_data)
+        
+        if self.pp_object:
+            preprocessed_corpus = self.pp_object.preprocess(self.training_data)
+        else:
+            preprocessed_corpus = self.training_data
 
         self.vectorizer = TfidfVectorizer()
         X_tfidf = self.vectorizer.fit_transform(preprocessed_corpus)
@@ -519,8 +522,12 @@ class LSA_Model():
             ID = int(self.df[self.df[self.column]==ID].ID)
 
         original_phrase = self.df[self.df['ID']==ID].texto.values
+        
+        if self.pp_object:
+            preprocessed_phrase = self.pp_object.preprocess(original_phrase)
+        else:
+            preprocessed_phrase = original_phrase
 
-        preprocessed_phrase = self.pp_object.preprocess(original_phrase)
 
         input_vector = self.vectorizer.transform(preprocessed_phrase)
 
@@ -562,8 +569,11 @@ class LSA_Model():
         a. Dict of N most similar phrases, with ID as keys and texts + score as values.
 
         """
-
-        pp_data = self.pp_object.preprocess(new_document)
+        
+        if self.pp_object:
+            pp_data = self.pp_object.preprocess(new_document)
+        else:
+            pp_data = new_document
 
         input_vector = self.vectorizer.transform(pp_data)
 
@@ -632,9 +642,12 @@ class LDA_Model():
         self.column = data_column
         
         self.training_data = self.df[self.column].tolist()
-    
-        preprocessed_corpus = self.pp_object.preprocess(self.training_data)
-
+        
+        if self.pp_object:
+            preprocessed_corpus = self.pp_object.preprocess(self.training_data)
+        else:
+            preprocessed_corpus = self.training_data
+            
         self.vectorizer = TfidfVectorizer()
         X_tfidf = self.vectorizer.fit_transform(preprocessed_corpus)
 
@@ -663,8 +676,11 @@ class LDA_Model():
             ID = int(self.df[self.df[self.column]==ID].ID)
 
         original_phrase = self.df[self.df['ID']==ID].texto.values
-
-        preprocessed_phrase = self.pp_object.preprocess(original_phrase)
+        
+        if self.pp_object:
+            preprocessed_phrase = self.pp_object.preprocess(original_phrase)
+        else:
+            preprocessed_phrase = original_phrase
 
         input_vector = self.vectorizer.transform(preprocessed_phrase)
 
@@ -706,8 +722,11 @@ class LDA_Model():
         a. Dict of N most similar phrases, with ID as keys and texts + score as values.
 
         """
-
-        pp_data = self.pp_object.preprocess(new_document)
+        
+        if self.pp_object:
+            pp_data = self.pp_object.preprocess(new_document)
+        else:
+            pp_data = new_document
 
         input_vector = self.vectorizer.transform(pp_data)
 
@@ -730,6 +749,261 @@ class LDA_Model():
             simil_dict[original_ID] = [original_text, score]
 
             print(f'----\n{i}.\n{original_text}\nScore: {score}')
+
+
+        return simil_dict
+    
+    
+"""
+    9. Semantic Similarity: LDA
+"""
+
+import itertools
+
+import itertools
+
+class Spacy_Model():
+
+    """
+        Find top N most similars, for new or existing (in df) documents using Spacy's es_core_news_lg model.
+        
+        -----
+        Input: 
+        a. df: pandas DataFrame with at least this columns: 
+            - ID (int): to identify the document
+            - data_column (str)
+        
+        b. Preprocess object
+        
+        ----
+
+    """
+    
+    def __init__(self, df, pp_object = None):
+        self.df = df
+        self.pp_object = pp_object
+        self.nlp = spacy.load('es_core_news_lg')
+        self.trained = False
+        
+        
+    def preprocess_training_data(self, data_column = 'texto'):
+        
+        self.training_data = self.df[data_column].tolist()
+        
+        if self.pp_object:
+            self.preprocessed_corpus = self.pp_object.preprocess(self.training_data)
+        else:
+            self.preprocessed_corpus = self.training_data
+        
+        
+    def train(self, data_column = 'texto'):
+
+        """
+        Only necessary for predict_old_trained (faster than predict_old_non_trained).
+
+        ----
+        Input:
+        a. data_column: column to train on (that is, calculate similarities).
+        
+        ----
+        Output:
+        a. df_simil: matrix with similarities between every possible pair of documents.
+
+        """
+        self.trained = True
+        
+        self.column = data_column
+        
+        self.preprocess_training_data(self.column)
+        
+        # Create a list of tuples containing all possible pairs of strings and ID's
+        doc_pairs = list(itertools.combinations(self.preprocessed_corpus, 2))
+        
+        # Calculate similarity for each pair
+        similarities = []
+        for i, pair in enumerate(doc_pairs):
+            doc1 = self.nlp(pair[0])
+            doc2 = self.nlp(pair[1])
+            sim = doc1.similarity(doc2)
+            similarities.append(sim)
+            if i%15000==0:print(f'Training {round(len(similarities)/len(doc_pairs)*100)}%\n')
+                
+        # Construct dataframe
+        self.IDs = self.df['ID'].tolist()
+        
+        self.df_simil = pd.DataFrame(index=self.IDs, columns=self.IDs)
+        
+        ID_pairs = list(itertools.combinations(self.IDs, 2))
+
+        # Fill the dataframe with the calculated similarities
+        for i, pair in enumerate(ID_pairs):
+            self.df_simil.loc[pair[0], pair[1]] = similarities[i]
+            self.df_simil.loc[pair[1], pair[0]] = similarities[i]
+        self.df_simil.fillna(1.0, inplace=True)
+    
+        return self.df_simil
+    
+    
+    def predict_old_trained(self, ID, N=10):
+
+        """
+        Finds top N most similar existing phrases with trained model.
+
+        ----
+        Input:
+        a. ID: existing phrase (from column 'texto') or ID (from column 'ID').
+        b. N: int (Top N)
+
+        ------
+        Ouput:
+        a. Dict of N most similar phrases, with ID as keys and texts + score as values.
+
+        """
+
+        # check if input is ID or phrase
+        if ID not in self.df['ID'].values:
+            ID = int(self.df[self.df[self.column]==ID].ID)
+
+        original_phrase = self.df[self.df['ID']==ID].texto.values
+        
+        if self.pp_object:
+            preprocessed_phrase = self.pp_object.preprocess(original_phrase)
+        else:
+            preprocessed_phrase = original_phrase
+
+        
+        index_and_scores = self.df_simil[ID].nlargest(N+1)[1:]
+
+        top_scores = index_and_scores.values
+
+        top_indexes = index_and_scores.index
+
+        top_texts = self.df[self.df['ID'].isin(top_indexes)].texto.tolist()
+
+        print(f'Comparing:\n{original_phrase}\n')
+
+        simil_dict = {}
+
+        for i in range(N):
+
+            # create dict
+            simil_dict[top_indexes[i]] = [top_texts[i], top_scores[i]]
+
+            print(f'----\n{i}.\n{top_texts[i]}\nScore: {top_scores.tolist()[i]}')
+
+        return simil_dict 
+    
+    
+    def predict_old_non_trained(self, ID, data_column = 'texto', N=10):
+
+        """
+        Finds top N most similar existing phrases with non trained model.
+
+        ----
+        Input:
+        a. ID: existing phrase (from column 'texto') or ID (from column 'ID').
+        b. N: int (Top N)
+
+        ------
+        Ouput:
+        a. Dict of N most similar phrases, with ID as keys and texts + score as values.
+
+        """
+        
+        self.column = data_column
+        
+        # check if input is ID or phrase
+        if ID not in self.df['ID'].values:
+            ID = int(self.df[self.df[self.column]==ID].ID)
+            
+        original_phrase = self.df[self.df['ID']==ID].texto.values
+        
+        
+        if self.pp_object:
+            preprocessed_phrase = self.pp_object.preprocess(original_phrase)[0]
+        else:
+            preprocessed_phrase = original_phrase
+
+        preprocessed_phrase_nlp = self.nlp(preprocessed_phrase)
+        
+        best_simil = {}
+        for i, doc in enumerate(self.preprocessed_corpus):
+            doc_nlp = self.nlp(doc)
+            original_phrase = self.df['texto'].tolist()[i]
+            best_simil[original_phrase] = preprocessed_phrase_nlp.similarity(doc_nlp)
+        
+        sorted_best_simil = sorted(best_simil.items(), key=lambda x: x[1], reverse=True)
+
+        print(f'Comparing:\n{original_phrase}\n')
+        
+        simil_dict = {}
+
+        for i, (phrase, score) in enumerate(sorted_best_simil[:N]):
+            
+            original_ID = self.df['ID'].tolist()[i]
+
+            simil_dict[original_ID] = [phrase, score]
+
+            print(f'----\n{i}.\n{phrase}\nScore: {score}')
+
+
+        return simil_dict
+    
+    
+    def predict_old(self, ID, data_column = 'texto', N=10):
+        
+        if self.trained:
+            print('Predicting with fast model.')
+            self.predict_old_trained(ID = ID, N=N)
+            
+        else:
+            print('Predicting with slow model.')
+            self.preprocess_training_data(data_column)
+            self.predict_old_non_trained(ID = ID, data_column = data_column, N=N)
+            
+    
+    def predict_new(self, new_document, N=10):
+
+        """
+        Finds top N most similar phrases.
+
+        ----
+        Input:
+        a. new_document (str): new phrase
+        b. N: int (Top N)
+
+        ------
+        Ouput:
+        a. Dict of N most similar phrases, with ID as keys and texts + score as values.
+
+        """
+        
+        if self.pp_object:
+            pp_data = self.pp_object.preprocess(new_document)
+        else:
+            pp_data = new_document
+
+        new_document_nlp = self.nlp(new_document)
+
+        best_simil = {}
+        for i, doc in enumerate(self.preprocessed_corpus):
+            doc_nlp = self.nlp(doc)
+            original_phrase = self.df['texto'].tolist()[i]
+            best_simil[original_phrase] = new_document_nlp.similarity(doc_nlp)
+        
+        sorted_best_simil = sorted(best_simil.items(), key=lambda x: x[1], reverse=True)
+
+        print(f'Comparing:\n{new_document}\n')
+        
+        simil_dict = {}
+
+        for i, (phrase, score) in enumerate(sorted_best_simil[:N]):
+            
+            original_ID = self.df['ID'].tolist()[i]
+
+            simil_dict[original_ID] = [phrase, score]
+
+            print(f'----\n{i}.\n{phrase}\nScore: {score}')
 
 
         return simil_dict
